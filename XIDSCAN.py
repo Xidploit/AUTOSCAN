a78c01374ec696a2d2b061d1e5ec427eead7e46a
Mr_XiD <44462886+Xidploit@users.noreply.github.com>
2019-02-06 22:16:08 +0700

    Update XIDSCAN.py

--- XIDSCAN.py
+++ XIDSCAN.py
@@ -8,12 +8,14 @@ import sys
 d3=os.system("curl http://ipinfo.io/ip")
 os.system("clear && clear && clear")
 logo = '''\033[0m 
- m    m mmmmm  mmmm    mmmm    mmm    mm   mm   m
-  #  #    #    #   "m #"   " m"   "   ##   #"m  #
+ m    m mmmmm  mmmm      mmmm   mmm    mm   mm   m
+  #  #    #    #   "m  #"   " m"   "   ##   #"m  #
    ##     #    #    # "#mmm  #       #  #  # #m #
-  m""m    #    #    #     "# #       #mm#  #  # #
- m"  "m mm#mm  #mmm"  "mmm#"  "mmm" #    # #   ## \033[0m  \033[91m    \033[1m 
-            [/> Coded By: MR_XID <\]
+  m""m    #    #    #    "# #       #mm#  #  # #
+ m"  "m mm#mm  #mmm" "mmm#" "mmm" #    # #   ## \033[0m  \033[91m    \033[1m 
+           Copyright ©2019 mr_xid
+	   
+	   [/> Coded By: MR_XID <\]
          {/>THANKS TO: D35TROY SQUAD<\}                               
      '''
 menu = '''\033[0m
@@ -29,7 +31,7 @@ menu = '''\033[0m
 print logo
 print menu
 def quit():
-            con = raw_input('Continue [Y/n] -> ')
+            con = raw_input('Lanjutkan[Y/n]/> ')
             if con[0].upper() == 'N':
                 exit()
             else:
@@ -40,81 +42,68 @@ def quit():
            
 def  select():
   try:
-    choice = input("Crips~# ")
+    choice = input("MR_XID/> ")
     if choice == 1:
-      d3 = raw_input('Enter IP Or Domain : ')
+      d3 = raw_input('Masukan Ip atau Domain : ')
       os.system("clear")
       print("""
- _       ____  ______  _________
-| |     / / / / / __ \/  _/ ___/
-| | /| / / /_/ / / / // / \__ \ 
-| |/ |/ / __  / /_/ _/ / ___/ / 
-|__/|__/_/ /_/\____/___//____/                                  
+ __  _____ ____   __        ___   _  ___ ___ ____
+\ \/ /_ _|  _ \  \ \      / / | | |/ _ \_ _/ ___|
+ \  / | || | | |  \ \ /\ / /| |_| | | | | |\___ \
+ /  \ | || |_| |   \ V  V / |  _  | |_| | | ___) |
+/_/\_\___|____/     \_/\_/  |_| |_|\___/___|____/                                  
       """)
       os.system("curl http://api.hackertarget.com/whois/?q=" + d3)
       print("")
       quit()
-    elif choice == 0:
-	  os.system("clear")
-	  print("This tool is only available for Linux and similar systems  ")
-	  os.system("git clone https://github.com/Manisso/Crips.git")
-	  os.system("cd Crips && sudo bash ./update.sh")
-	  os.system("crips")
     elif choice == 2:
-      d3 = raw_input('Enter IP Or Domain : ')
+      d3 = raw_input('Masukan Ip atau Domain : ')
       os.system("clear")
       print("""
- ____ ____   __   ___ ____ ____ _____ __  __ ____ ____ 
-(_  _(  _ \ /__\ / __( ___(  _ (  _  (  )(  (_  _( ___)
-  )(  )   //(__)( (__ )__) )   /)(_)( )(__)(  )(  )__) 
- (__)(_)\_(__)(__\___(____(_)\_(_____(______)(__)(____)
+__  _____ ____    _____ ____      _    ____ _____ ____             _
+\ \/ /_ _|  _ \  |_   _|  _ \    / \  / ___| ____|  _ \ ___  _   _| |_ ___
+ \  / | || | | |   | | | |_) |  / _ \| |   |  _| | |_) / _ \| | | | __/ _ \
+ /  \ | || |_| |   | | |  _ <  / ___ \ |___| |___|  _ < (_) | |_| | ||  __/
+/_/\_\___|____/    |_| |_| \_\/_/   \_\____|_____|_| \_\___/ \__,_|\__\___|
 """)
       os.system("curl https://api.hackertarget.com/mtr/?q=" + d3 )
       print("")
       quit()
     elif choice == 3:
-      d3 = raw_input('Enter Domain : ')
+      d3 = raw_input('Masukan Domain : ')
       os.system("clear")
       print("""
-______ _   _ _____   _                 _                
-|  _  | \ | /  ___| | |               | |               
-| | | |  \| \ `--.  | |     ___   ___ | | ___   _ _ __  
-| | | | . ` |`--. \ | |    / _ \ / _ \| |/ | | | | '_ \ 
-| |/ /| |\  /\__/ / | |___| (_) | (_) |   <| |_| | |_) |
-|___/ \_| \_\____/  \_____/\___/ \___/|_|\_\\__,_| .__ / 
-                                                 | |    
-                                                 |_|     
+__  _____ ____    ____  _   _ ____    _     ___   ___   ____ _  ___   _ ____
+\ \/ /_ _|  _ \  |  _ \| \ | / ___|  | |   / _ \ / _ \ / ___| |/ / | | |  _ \
+ \  / | || | | | | | | |  \| \___ \  | |  | | | | | | | |   | ' /| | | | |_) |
+ /  \ | || |_| | | |_| | |\  |___) | | |__| |_| | |_| | |___| . \| |_| |  __/
+/_/\_\___|____/  |____/|_| \_|____/  |_____\___/ \___/ \____|_|\_\\___/|_|
 """)
       os.system("curl http://api.hackertarget.com/dnslookup/?q=" + d3 )
       print("")
       quit()    
-    elif choice == 0:
-      print "Bye Bye"
-      os.system("clear")
-      exit()
     elif choice == 4:
-	  d3 = raw_input('Enter IP Address - IP Range Or Domain  : ')
+	  d3 = raw_input('Masukan IP Address - IP Range Or Domain  : ')
 	  os.system("clear")
 	  print("""
- _____                            ____  _____ _____ 
-| __  |___ _ _ ___ ___ ___ ___   |    \|   | |   __|
-|    -| -_| | | -_|  _|_ -| -_|  |  |  | | | |__   |
-|__|__|___|\_/|___|_| |___|___|  |____/|_|___|_____|
-                                                    
+__  _____ ____                                       ____  _   _ ____
+\ \/ /_ _|  _ \   _ __ _____   _____ _ __ ___  ___  |  _ \| \ | / ___|
+ \  / | || | | | | '__/ _ \ \ / / _ \ '__/ __|/ _ \ | | | |  \| \___ \
+ /  \ | || |_| | | | |  __/\ V /  __/ |  \__ \  __/ | |_| | |\  |___) |
+/_/\_\___|____/  |_|  \___| \_/ \___|_|  |___/\___| |____/|_| \_|____/
 	  """)
 	  os.system("curl https://api.hackertarget.com/reversedns/?q=" + d3 )
 	  print("")
 	  quit()
     elif choice == 5:
-	  d3 = raw_input('Enter IP Or Domain : ')
+	  d3 = raw_input('Masukan Ip atau Domain : ')
 	  os.system("clear")
 	  print("""
-   _____           _____ _____  
-  / ____|         |_   _|  __ \ 
- | |  __  ___  ___  | | | |__) |
- | | |_ |/ _ \/ _ \ | | |  ___/ 
- | |__| |  __| (_) _| |_| |     
-  \_____|\___|\___|_____|_|     
+__  _____ ____     ____       ___  _ ____
+\ \/ /_ _|  _ \   / ___| ___ / _ \(_)  _ \
+ \  / | || | | | | |  _ / _ \ | | | | |_) |
+ /  \ | || |_| | | |_| |  __/ |_| | |  __/
+/_/\_\___|____/   \____|\___|\___/|_|_|     
                                 	
 	""")
 	  os.system("curl http://api.hackertarget.com/geoip/?q=" + d3 )
@@ -122,32 +111,29 @@ ______ _   _ _____   _                 _
 	  print("")
 	  quit()
     elif choice == 6:
-      d3 = raw_input('Enter IP Or Domain : ')
+      d3 = raw_input('Masukan Ip atau Domain: ')
       os.system("clear")
       print("""
-     __                         __                 
-  /\ \ \_ __ ___   __ _ _ __   / _\ ___ __ _ _ __  
- /  \/ | '_ ` _ \ / _` | '_ \  \ \ / __/ _` | '_ \ 
-/ /\  /| | | | | | (_| | |_) | _\ | (_| (_| | | | |
-\_\ \/ |_| |_| |_|\__,_| .__/  \__/\___\__,_|_| |_|
-                       |_|                         
+  __  _____ ____    _   _                      ____
+  \ \/ /_ _|  _ \  | \ | |_ __ ___   __ _ _ __/ ___|  ___ __ _ _ __
+   \  / | || | | | |  \| | '_ ` _ \ / _` | '_ \___ \ / __/ _` | '_ \
+   /  \ | || |_| | | |\  | | | | | | (_| | |_) |__) | (_| (_| | | | |
+  /_/\_\___|____/  |_| \_|_| |_| |_|\__,_| .__/____/ \___\__,_|_| |_|
+                                        |_| 
       """)
       os.system("curl http://api.hackertarget.com/nmap/?q=" + d3 )
       print("")
       quit()
     elif choice == 7:
-	  d3 = raw_input('Enter IP Or Domain : ')
+	  d3 = raw_input('Masukan Ip atau Domain : ')
 	  os.system("clear")
 	  print("""
- (   (      (                             
- )\ ))\ )   )\ )            )             
-(()/(()/(  (()/(         ( /(   (         
- /(_)/(_))  /(_)) (   (  )\()) ))\ `  )   
-(_))(_))   (_))   )\  )\((_)\ /((_)/(/(   
-|_ _| _ \  | |   ((_)((_| |(_(_))(((_)_\  
- | ||  _/  | |__/ _ / _ | / /| || | '_ \) 
-|___|_|    |____\___\___|_\_\ \_,_| .__/  
-                                  |_|     
+__  _____ ____    ______     ___       _                     _
+\ \/ /_ _|  _ \  |  _ \ \   / (_)_ __ | |    ___   ___   ___| | ___   _ _ __
+ \  / | || | | | | |_) \ \ / /| | '_ \| |   / _ \ / _ \ / __| |/ / | | | '_ \
+ /  \ | || |_| | |  _ < \ V / | | |_) | |__| (_) | (_) | (__|   <| |_| | |_) |
+/_/\_\___|____/  |_| \_\ \_/  |_| .__/|_____\___/ \___/ \___|_|\_\\__,_| .__/
+                                |_|                                    |_|
 	  """)
 	  os.system("wget http://api.hackertarget.com/reverseiplookup/?q=" + d3 )
 	  os.system("clear")
